00839ecaf077329bb5ab3a50814f83be05fb1d6b
unknown <ar.mulla99@gmail.com>
2025-08-03 11:41:27 -0700

    Added course info 1.1 and 1.2

---
+++ courseinformation/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const part1 = "Fundamentals of React";
+const exercises1 = 10;
+const part2 = "Using props to pass data";
+const exercises2 = 7;
+const part3 = "State of a component";
+const exercises3 = 14;
+const course = "Half Stack application development";
+
+const Header = () => {
+  return (<h1>{course}</h1>)
+};
+
+
+const Content = () => {
+  return (
+    <>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </>
+  );
+};
+const Total = () => {
+  return (<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>)
+};
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Total />
+    </div>
+  );
+};
+
+export default App;
